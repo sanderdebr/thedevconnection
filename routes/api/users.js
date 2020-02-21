0a6e613689a55d4d916a416fd2bbafa0822ed6ce
sanderdebr <sander606@gmail.com>
2020-02-21 19:11:15 +0100

    added some routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 
 const User = require("../../models/User");
@@ -34,6 +36,7 @@ router.post(
     try {
       let user = await User.findOne({ email });
 
+      // Check if user exists
       if (user) {
         return res
           .status(400)
@@ -53,14 +56,30 @@ router.post(
         password
       });
 
+      // Generate password
       const salt = await bcrypt.genSalt(10);
 
       user.password = await bcrypt.hash(password, salt);
 
+      // Store user in DB
       await user.save();
 
-      // Return jsonwebtoken
-      res.send("User registered");
+      // Add JWT
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server error");
