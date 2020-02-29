54eaa2a48800e0894be3860029228eef0d43d5d7
sanderdebr <sander606@gmail.com>
2020-02-29 17:44:58 +0100

    setup clientside react

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  const defaultState = {
+    email: "",
+    password: ""
+  };
+
+  const [formData, setFormData] = useState(defaultState);
+  const { email, password } = formData;
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    console.log("success");
+  };
+
+  return (
+    <section className="container">
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Sign Into Your Accoutn
+      </p>
+      <form className="form" onSubmit={e => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            value={email}
+            onChange={e => onChange(e)}
+            name="email"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            value={password}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Register</Link>
+      </p>
+    </section>
+  );
+};
+
+export default Login;
